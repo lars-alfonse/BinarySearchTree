412b7e3f86d41e1d1be91dd0faadc687882f68b7
lars-alfonse <lars.alfonse@gmail.com>
2017-08-22 15:33:46 -0500

    added binarytree and node classes

--- BinarySearch/BinarySearchTree.cs
+++ BinarySearch/BinarySearchTree.cs
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace BinarySearch
 {
-    class BinarySearchTree
+    class BinarySearchTree<T> where T : IComparable
     {
+        private Node<T> root;
+
+
+
+        public void AddNode(Node<T> node)
+        {
+            if (root == null)
+            {
+                root = node;
+            }
+            else if (node.data.CompareTo(root.data) > 0)
+            {
+                if (root.rightChild == null)
+                {
+                    root.rightChild = node;
+                }
+                else
+                {
+                    AddNode(node, root.rightChild);
+                    return;
+                }
+            }
+            else if (node.data.CompareTo(root.data) < 0)
+            {
+                if (root.leftChild == null)
+                {
+                    root.leftChild = node;
+                }
+                else
+                {
+                    AddNode(node, root.leftChild);
+                    return;
+                }
+            }
+        }
+        public void AddNode(Node<T> node, Node<T> check)
+        {
+            if (check == null)
+            {
+                check = node;
+            }
+            else if (node.data.CompareTo(check.data) > 0)
+            {
+                if (check.rightChild == null)
+                {
+                    check.rightChild = node;
+                }
+                else
+                {
+                    AddNode(node, check.rightChild);
+                    return;
+                }
+            }
+            else if (node.data.CompareTo(check.data) < 0)
+            {
+                if (check.leftChild == null)
+                {
+                    check.leftChild = node;
+                }
+                else
+                {
+                    AddNode(node, check.leftChild);
+                    return;
+                }
+            }
+        }
     }
 }
--- BinarySearch/Node.cs
+++ BinarySearch/Node.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace BinarySearch
 {
-    class Node
+    class Node<T> where T : IComparable
     {
+        public T data;
+
+        public Node<T> leftChild;
+        public Node<T> rightChild;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
     }
+
 }
